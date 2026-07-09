4395589c8f2b3d8e806911e947b1b01b2a1c8193
fyansyarafa <abuluqman19@hotmail.com>
2026-07-09 15:14:15 +0700

    add packages.yml for dbt utils, and test on actor silver model

--- pagila_analytics/models/silver/silver_stream_pagila_actor.sql
+++ pagila_analytics/models/silver/silver_stream_pagila_actor.sql
@@ -1,14 +1,13 @@
 select 
-{{
-    dbt_utils.star(
-        from=source('source_pagila', 'stream_pagila_actor'),
-        except=["__deleted"]
-    )
-}}
-
-{{
-    audit_columns()
-}}
+    {{
+        dbt_utils.star(
+            from=source('source_pagila', 'stream_pagila_actor'),
+            except=["synced_at", "__deleted"]
+        )
+    }},
+    {{
+        audit_columns()
+    }}
 
 from {{
     source('source_pagila', 'stream_pagila_actor')
