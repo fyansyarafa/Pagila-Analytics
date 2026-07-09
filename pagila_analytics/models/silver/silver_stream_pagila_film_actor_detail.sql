69210a52ef7efecef56a2dc05aabd58bb3ac6500
fyansyarafa <abuluqman19@hotmail.com>
2026-07-09 19:16:01 +0700

    new silver model and config properties.yml

--- pagila_analytics/models/silver/silver_stream_pagila_film_actor_detail.sql
+++ pagila_analytics/models/silver/silver_stream_pagila_film_actor_detail.sql
@@ -29,10 +29,26 @@ with film_actor as (
   from {{
       ref('silver_stream_pagila_film')
     }}
+), film_category as (
+    select 
+        film_id,
+        category_id
+    from {{
+        ref('silver_stream_pagila_film_category')
+    }}
 )
+
 select 
   fa.actor_id, 
   fa.film_id, 
+  fc.category_id,
+  {{
+    dbt_utils.generate_surrogate_key([
+        'fa.actor_id',
+        'fa.film_id',
+        'fc.category_id'
+    ])
+  }} as film_detail_sk,
   a.full_name, 
   f.title, 
   f.description, 
@@ -45,6 +61,8 @@ select
   f.replacement_cost, 
   f.rating,
   now() as __audit_dbt_refresh_date
+
 from film_actor fa
 left join actor a on fa.actor_id = a.actor_id
 left join film f on fa.film_id = f.film_id
+left join film_category fc on f.film_id = fc.film_id
