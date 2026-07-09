86d1e18779f0159d06b9997344ff5216e862c217
fyansyarafa <abuluqman19@hotmail.com>
2026-07-09 18:18:46 +0700

    actor: add full name, delete first name, last name

--- pagila_analytics/analyses/worksheet.sql
+++ pagila_analytics/analyses/worksheet.sql
@@ -1,11 +1,14 @@
-  select
-    actor_id,
-    first_name,
-    last_name
-  from {{ source('source_pagila', 'stream_pagila_actor') }}
 
-select * 
-from {{
-    source('source_pagila', 'stream_pagila_actor')
-}}
-where __deleted = 'false'
+with film_actor as (
+  select 
+    actor_id, 
+    film_id
+  from {{
+    ref('silver_stream_pagila_film_actor')
+  }}
+), actor as (
+select
+  actor_id,
+  first_name,
+from 
+)
--- pagila_analytics/models/silver/silver_stream_pagila_actor.sql
+++ pagila_analytics/models/silver/silver_stream_pagila_actor.sql
@@ -2,9 +2,10 @@ select
     {{
         dbt_utils.star(
             from=source('source_pagila', 'stream_pagila_actor'),
-            except=["synced_at", "__deleted"]
+            except=["synced_at", "__deleted", "first_name", "last_name"]
         )
     }},
+    first_name || ' ' || last_name as full_name,
     {{
         audit_columns()
     }}
