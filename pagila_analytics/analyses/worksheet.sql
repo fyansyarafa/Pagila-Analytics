9fc3ceb5fbc11a63607fb73153a5c2f7180e1eff
fyansyarafa <abuluqman19@hotmail.com>
2026-07-09 14:27:04 +0700

    lupa

--- pagila_analytics/analyses/worksheet.sql
+++ pagila_analytics/analyses/worksheet.sql
@@ -1,1 +1,16 @@
-select * from {{ source('source_pagila', 'stream_pagila_actor') }}
+with actor as (
+  select
+    actor_id,
+    first_name,
+    last_name
+  from {{ source('source_pagila', 'stream_pagila_actor') }}
+), film as(
+  select 
+    film_id,
+    title
+  from {{ source('source_pagila', 'stream_pagila_film') }}
+    
+)
+select * 
+from actor a
+join film f on a.film_id = f.film_id
