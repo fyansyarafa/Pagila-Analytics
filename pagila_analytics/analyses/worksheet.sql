fc18b77b2b8306af05eed8a63b2f862dbe2c9743
fyansyarafa <abuluqman19@hotmail.com>
2026-07-11 17:56:56 +0700

    add relational alias on macros

--- pagila_analytics/analyses/worksheet.sql
+++ pagila_analytics/analyses/worksheet.sql
@@ -1,8 +1,52 @@
-select
-{{
-  exclude_column(
-    ref("silver_stream_pagila_actor"),
-    ["actor_id", "full_name"]
-  )
-}}
-from {{ ref("silver_stream_pagila_actor") }}
+with store as (
+  select 
+    store_id,
+    manager_staff_id, 
+    address_id
+  from {{ ref("silver_stream_pagila_store") }}
+), inventory as (
+  select 
+    inventory_id,
+    film_id, 
+    store_id
+  from {{ ref("silver_stream_pagila_inventory") }}
+), staff as (
+  select 
+    staff_id,
+    first_name || ' ' || last_name as staff_full_name,
+    email,
+    active
+  from {{ ref("silver_stream_pagila_staff") }}
+), address_table as (
+  select 
+    address_id,
+    address,
+    address2,
+    postal_code,
+    phone,
+    city,
+    country
+  from {{ ref("silver_stream_pagila_address_detail") }}
+)
+
+select 
+  s.store_id,
+  sf.staff_id as manager_staff_id,
+  s.address_id,
+  i.inventory_id,
+  i.film_id,
+  sf.staff_full_name,
+  sf.active as staff_active,
+  sf.email as staff_email,
+  {{
+    exclude_column(
+      ref("silver_stream_pagila_address_detail"),
+      ["address_id"],
+      relation_alias="ad"
+    )
+  }}
+from
+store s 
+left join inventory i on s.store_id = i.store_id
+left join staff sf on s.manager_staff_id = sf.staff_id
+left join address_table ad on ad.address_id = s.address_id
--- pagila_analytics/macros/exclude_columns.sql
+++ pagila_analytics/macros/exclude_columns.sql
@@ -1,3 +1,3 @@
-{% macro exclude_column(model, columns) %}
-    {{ dbt_utils.star(from=model, except=columns) }}
+{% macro exclude_column(model, columns, relation_alias=none) %}
+    {{ dbt_utils.star(from=model, except=columns, relation_alias=relation_alias) }}
 {% endmacro %}
